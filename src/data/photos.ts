4d9cede6285329184a6be50f70ea7383ecd69b57
Kyle Chang <hi@yiyu0x.org>
2025-04-05 17:49:21 -0700

    fix: resolve issue with images not displaying correctly

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,27 +1,27 @@
 export const photos = [
   {
-    image: "/src/assets/images/1.jpg",
+    image: "/images/1.jpg",
     date: "March 2025",
     location: "Santa Monica, CA",
     caption: "",
     style: "sm:top-4 sm:left-[-10%] top-2 left-2 rotate-[-6deg] z-10"
   },
   {
-    image: "/src/assets/images/2.jpg",
+    image: "/images/2.jpg",
     date: "December 2024",
     location: "New York City, NY",
     caption: "",
     style: "sm:top-12 sm:left-[15%] top-8 right-2 rotate-[4deg] z-20"
   },
   {
-    image: "/src/assets/images/3.jpg",
+    image: "/images/3.jpg",
     date: "December 2024",
     location: "Boston, MA",
     caption: "",
     style: "sm:left-[45%] top-8 right-8 rotate-[-3deg] z-30"
   },
   {
-    image: "/src/assets/images/4.jpg",
+    image: "/images/4.jpg",
     date: "September 2024",
     location: "Seattle, WA",
     caption: "",
